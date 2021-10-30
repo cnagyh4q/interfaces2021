f337d3e978909d264dd4603b7faa08dd2f95a4c3
Christian <cnagy@technisys.com>
2021-10-29 23:48:25 -0300

    cambios a ultimo momento

--- TP3/EntregaFinal/js/script.js
+++ TP3/EntregaFinal/js/script.js
@@ -39,39 +39,65 @@ document.addEventListener("DOMContentLoaded" , (e) =>{
 
     cambioEscena(escena);
 
-    document.querySelector("#selectEscena").addEventListener("change" , (e) => {
+    
 
-        escena = e.target.value;               
-       
-        cambioEscena(escena);
+    document.querySelector("#escena2").addEventListener("click",(e)=>{
 
+        cambioEscena(2);
+        e.target.classList.toggle("resaltado");
+        document.querySelector("#escena1").classList.toggle("resaltado");
 
     })
 
 
+    document.querySelector("#escena1").addEventListener("click",(e)=>{
+
+        cambioEscena(1);
+        e.target.classList.toggle("resaltado");
+        document.querySelector("#escena2").classList.toggle("resaltado");
+
+    })
+
+
+
+
+    document.querySelector("#btn-iniciar").addEventListener("click" , (e)=>{
+
+        juego.setJuegoEmpezado();
+        juego.setEnvent("pressClick");
+        juego.iniciar();
+        e.target.disabled=true;
+        document.querySelector("#btn-reiniciar").disabled=false;
+        
+
+    
+    })
+
+
+    document.querySelector("#btn-reiniciar").addEventListener("click" , (e)=>{
+
+        
+        juego.reiniciarJuego();
+
+        document.querySelector("#btn-iniciar").disabled=false;
+        e.target.disabled=true;
+        
+
+    
+    })
 
 
     document.addEventListener("click" , ()=>{
 
-        juego.setJuegoEmpezado();
-        juego.setEnvent("pressClick");
+           juego.setEnvent("pressClick");
         
-        console.log("Boton DOM Click");
+        
 
     
     })
 
-/*
-    document.addEventListener("dblclick" , ()=>{
-
-        juego.setEnvent("dobleClick");
-        console.log("DOBLE CLICK Click");
-    
-    })
-  */  
-
-   
-     juego.iniciar();
+  
+     
 
      setInterval(()=>{
          document.querySelector("#puntaje").innerHTML= juego.getPuntaje();
